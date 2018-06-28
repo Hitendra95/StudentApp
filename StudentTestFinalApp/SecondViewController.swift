c3cb4d4bb92953e74d866804b9fe6fc104c93e3a
Hitendra Dubey <hd.hitendradubey@gmail.com>
2018-06-28 16:39:33 +0530

    Initial Commit

---
+++ StudentTestFinalApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StudentTestFinalApp
+//
+//  Created by Hitendra Dubey on 28/06/18.
+//  Copyright © 2018 Hitendra Dubey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
